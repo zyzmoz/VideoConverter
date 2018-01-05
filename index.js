46e733503434f7a09868a0eef4b3a4bf8dcd9cf6
Daniel Cunha <danielbrandaodacunha@gmail.com>
2018-01-05 17:23:28 -0200

    Almost done

---
+++ index.js
@@ -0,0 +1,62 @@
+const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+const _ = require('lodash');
+
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800,
+    webPreferences: { backgroundThrottling: false }
+  });
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+});
+
+ipcMain.on('conversion:start', (event, videos) => {
+  console.log(videos);
+  _.each(videos, video => {
+    const outputDir = video.path.split(video.name)[0];
+    const outputName = video.name.split('.')[0];
+    const outputPath = `${outputDir}${outputName}.${video.format}`;
+    ffmpeg(video.path)
+      .output(outputPath)
+      .on('progress', ({timemark}) => {
+        mainWindow.webContents.send('conversion:progress', {video, timemark});        
+      })
+      .on('end', () => {
+        mainWindow.webContents.send('conversion:end', {video, outputPath});
+      })
+      .run();
+  });
+});
+
+ipcMain.on('videos:added', (event, videos) =>{
+  const promises = _.map(videos, video => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        video.duration = metadata.format.duration
+        video.format = 'avi';
+        resolve(video);//...video get all the properties of the object
+      });
+    });
+  });
+  Promise.all(promises)
+    .then((results) => {
+      console.log(results);
+      mainWindow.webContents.send('metadata:complete', results);
+    });
+
+  // console.log(videos);
+  // const promise = new Promise((resolve, reject) => {
+  //   ffmpeg.ffprobe(videos[0].path, (err, metadata) => {
+  //     resolve(metadata);
+  //   });
+  // });
+  //
+  // promise.then((metadata) => {
+  //   console.log(metadata);
+  // });
+});
